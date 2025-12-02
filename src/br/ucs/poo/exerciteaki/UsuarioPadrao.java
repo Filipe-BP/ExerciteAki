7284e6d50b44dbcfc25805e3709fb005a9ece878
Samara Castro <sa_gomes@live.com>
2025-12-02 09:39:37 -0300

    classe utils e remoção de código não utilizado

--- src/br/ucs/poo/exerciteaki/UsuarioPadrao.java
+++
@@ -1,7 +0,0 @@
-package br.ucs.poo.exerciteaki;
-
-public class UsuarioPadrao extends Usuario {
-    public UsuarioPadrao(String login, String password, Boolean administrador) {
-        super(login, password, administrador);
-    }
-}
